0d9d8efd6be38960e0fc093ac3a17babc5f16de0
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-23 23:05:27 +0300

    Installing and defining Database

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from .forms import ReviewForm
-from ..models import Review
 
 
 
--- config.py
+++ config.py
@@ -4,6 +4,8 @@ class Config:
 
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wainainakasyoka:W41n41n4@localhost/pomodoro'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
